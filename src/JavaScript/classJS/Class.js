8948618eca51c6b108ba4d76ab22fdbda0a6820b
doctap <vasilij.alexsandrovich@gmail.com>
2023-08-13 16:41:52 +0600

    function for extending classes

--- src/JavaScript/classJS/Class.js
+++ src/JavaScript/classJS/Class.js
@@ -53,4 +53,31 @@ function Range2 (from, to) {
 // const r2 = new Range2(1, 5);
 // console.log(r2 instanceof Range);
 
+// === function for extending classes
+
+class Sirop {
+  constructor (r, e) {
+    this.r = r;
+    this.e = e;
+  }
+}
+
+function extensionFactory(Main) {
+  return class extends Main {
+    constructor (q, w) {
+      super(1, 2);
+      this.q = q;
+      this.w = w;
+    }
+  }
+}
+
+// const extendedClass = extensionFactory(Sirop);
+// const instanceExtendedSiropClass = new extendedClass(3, 4)
+
+// console.log(instanceExtendedSiropClass.r)
+// console.log(instanceExtendedSiropClass.e)
+// console.log(instanceExtendedSiropClass.q)
+// console.log(instanceExtendedSiropClass.w)
+
 // ===
